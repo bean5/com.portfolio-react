72cfd84f3526778d8192fefe2c60254873f06705
bean5 <2052646+bean5@users.noreply.github.com>
2024-04-15 14:13:11 -0600

    Update message

--- src/pages/Preface.js
+++ src/pages/Preface.js
@@ -7,7 +7,7 @@ class Preface extends Component {
         <p className="Preface">
           This is the future site of <a href="https://www.cancompute.tech/">https://www.cancompute.tech/</a>! Currently my website was built on a proprietary engine. I want to migrate it to ReactJS.</p>
         <p>
-          To view the code for this site, see <a href="https://github.com/bean5/react-portfolio">github.com/bean5/react-portfolio</a>.
+          This website is open sourced at <a href="https://github.com/bean5/react-portfolio">github.com/bean5/react-portfolio</a>.
           <br />
           <br />
           Live version is at <a href="https://portfolio-bean5.web.app/">portfolio-bean5.web.app/</a>
